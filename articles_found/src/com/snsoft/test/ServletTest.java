f3ec0b373b78c81f1219b24b9107c8e198ccb254
swk <1796050317@qq.com>
2017-07-21 21:27:05 +0800

    Java源码上传

---
+++ articles_found/src/com/snsoft/test/ServletTest.java
@@ -0,0 +1,28 @@
+package com.snsoft.test;
+
+import java.io.InputStream;
+import org.junit.Test;
+import com.snsoft.util.HttpUtils;
+
+
+public class ServletTest {
+	
+	@Test
+	public void servletPostTest(){
+		String url = "http://localhost:8080/articles_found/login";
+		InputStream is = HttpUtils.sendPost(url, "{account:'111111',password:'111111'}");
+		String result = HttpUtils.getString(is);
+		System.out.println(result);
+	}
+	
+	@Test
+	public void Test22(){
+		String url = "http://localhost:8080/articles_found/Test2";
+		InputStream is = HttpUtils.sendPost(url, "{saveName:'7/20/0ea05e8f4f4bb1303a374b97f7c9.jpg'}");
+		String result = HttpUtils.getString(is);
+		System.out.println(result);
+	}
+	
+	
+	
+}
